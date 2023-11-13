9717677ee46849229bad20d03ece69a3dbf0cbf6
21ITR124 <118598723+21ITR124@users.noreply.github.com>
2023-11-13 19:14:34 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/login", {
+    const response = await fetch("https://cb2-cq47.onrender.com/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
