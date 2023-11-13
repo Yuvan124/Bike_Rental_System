1d6a13cdd91973587db24bfaf39b720ac3e192be
21ITR124 <118598723+21ITR124@users.noreply.github.com>
2023-11-13 19:15:24 +0530

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -13,7 +13,7 @@ const Register = () => {
 
   const handleRegister = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/register", {
+    const response = await fetch("https://cb2-cq47.onrender.com/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
