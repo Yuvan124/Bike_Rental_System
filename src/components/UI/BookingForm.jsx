6de31111fa5be38dbf12bdafad8eb211bffb73ad
21ITR124 <118598723+21ITR124@users.noreply.github.com>
2023-11-14 13:17:35 +0530

    Update BookingForm.jsx

--- src/components/UI/BookingForm.jsx
+++ src/components/UI/BookingForm.jsx
@@ -38,7 +38,7 @@ const BookingAndPaymentForm = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/bookings", {
+      const response = await fetch("https://pbk.onrender.com/bookings", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
